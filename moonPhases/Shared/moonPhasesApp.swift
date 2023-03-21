373064b13f92f9a87274f2237bbf05c82eaec8c5
Abinaya Dinesh <abinayadinesh1@users.noreply.github.com>
2023-03-21 16:01:31 -0700

    Add files via upload

---
+++ moonPhases/Shared/moonPhasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonPhasesApp.swift
+//  Shared
+//
+//  Created by Abinaya on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct moonPhasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
